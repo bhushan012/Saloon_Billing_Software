28999fb8028a3c0188d5397ce5de85804381ef07
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2020-11-02 11:44:06 -0800

    add pie chart

--- footer.php
+++ footer.php
@@ -21,7 +21,11 @@ include "urlMapping.php";
             $z++;
         }
 
-        print_r($data);
+        $result = $operationInstance->getAllServices();
+        $i= 0 ;
+        while ($row = $result->fetch_assoc()) {
+            $services[$i] = $row['serviceName'];
+        }
 
 
 ?>
@@ -111,6 +115,22 @@ $(document).ready(function(){
                     }
                 }]
             }
+        }
+    });
+    // pie
+    var ctxP = document.getElementById("pieChart").getContext('2d');
+    var myPieChart = new Chart(ctxP, {
+        type: 'pie',
+        data: {
+            labels: [<?php foreach($services as $key => $value){ echo "'"; echo $value; echo "'";echo ",";} ?>],
+            datasets: [{
+                data: [300, 50, 100, 40, 120],
+                backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+                hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+            }]
+        },
+        options: {
+            responsive: true
         }
     });
 });
