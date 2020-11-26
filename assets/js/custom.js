f2ed57c8b9cf78bfda17f2584ca2f5f8ee6aa36d
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2020-11-26 12:17:27 -0800

    test

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -63,7 +63,7 @@ $(document).ready(function () {
         console.log(totalAmt + "totalAmt Price");
         $('#subTotal').empty().append(totalAmt - amtDiscounted);
     });
-    $("#discountPercent").on("keypress",function(){
+    $("#discountPercent").on("keyup",function(){
         console.log("1");
         var valueCheck = $(this).val();
         if(valueCheck > 99){
