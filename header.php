a53757892b2d5c31c9f092d001618761a7e1573a
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2020-12-03 11:17:09 -0800

    url

--- header.php
+++ header.php
@@ -50,7 +50,7 @@
         </div>
     </div>
     <nav id="one" class="d-none  navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="<?php echo $homeUrl?>"><img class="img-fluid" href="<?php echo $homeUrl.'assets/images/tranquil-Logo-icon.png'?>"></a>
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="<?php echo $homeUrl?>"><img class="img-fluid" href="<?php echo $homeUrl.'/assets/images/tranquil-Logo-icon.png'?>"></a>
         <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
