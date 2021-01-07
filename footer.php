321db63c62185bd379e6078b2cd6d8aab0a4a9c6
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-01-07 11:48:47 -0800

    check

--- footer.php
+++ footer.php
@@ -77,13 +77,14 @@ include "urlMapping.php";
   </div>
 </div>
 <!---Small Modal-->
-    <script src="<?= $homeUrl; ?>/assets/js/jquery-3.5.1.js" defer></script>
-    <script src="<?= $homeUrl; ?>/assets/autoComplete/autocomplete.min.js"></script>
+    <script src="<?= $homeUrl; ?>/assets/js/jquery-3.5.1.js" ></script>
+    <script src="<?= $homeUrl; ?>/assets/bootstrap-4.4.1/js/bootstrap.min.js"></script>
+    <script src="<?= $homeUrl; ?>/assets/autoComplete/autocomplete.min.js" ></script>
     <script src="<?= $homeUrl; ?>/assets/js/popper.min.js"></script>
     <script src="<?= $homeUrl; ?>/assets/js/mdb.min.js"></script>
     <script src="<?= $homeUrl; ?>/assets/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
     <!-- <script src="<?= $homeUrl; ?>/assets/fontawsome5.13.0/fontawesome.min.js"></script> -->
-    <script src="<?= $homeUrl; ?>/assets/bootstrap-4.4.1/js/bootstrap.min.js"></script>
+   
     <script src="<?= $homeUrl; ?>/assets/dataTables/js/addons/datatables.min.js"></script>
 
     <script src="<?= $homeUrl; ?>/assets/js/custom.js"></script>
