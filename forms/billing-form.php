13ca23203b75feadbd8c8d147276383e3db237be
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-01-07 00:57:16 -0800

    add credit

--- forms/billing-form.php
+++ forms/billing-form.php
@@ -181,6 +181,13 @@ if (isset($_GET['success'])) {
                         <input class="form-control p-3" id="amntPaid" type="text" placeholder="Amount Paid" />
                     </div>
                 </div>
+                <div class="col-md-4 mt-3">
+                    <div class="form-group text-center mb-2">
+                        <div class="input-group-btn">
+                            <button class="btn btn-success" id="addCredit" type="button"><i class="fa fa-plus"></i><span class="h6"> Add</span></button>
+                        </div>
+                    </div>
+                </div>
             </div>
             <?php
             $response = "";
