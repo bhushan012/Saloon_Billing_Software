75ef4fa507d4b492dd54dacd20fd5ef4bef2c8fd
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-11-17 18:38:32 +0530

    sub category on change fetch services

--- logic/getSubCategory.php
+++ logic/getSubCategory.php
@@ -4,7 +4,10 @@ $operationInstance = new Operations();
 $categoryId = $_POST['categoryId'];
 $result = $operationInstance->getSaloonSubCategoryById($categoryId);
 if ($result->num_rows > 0) {
+    echo '<option value="">Service Sub-Category</option>';
     while ($row = $result->fetch_assoc()) {
         echo "<option value=" . $row['sub_category_id'] . ">" . $row['sub_category_name'] . "</option>";
     }
+}else{
+    echo '<option value="">Service Sub-Category</option>';
 }
