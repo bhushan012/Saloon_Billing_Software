484795362a2e1d424ccb4373196c0bdda7759248
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-11-23 15:55:36 +0530

    testing user credit func

--- logic/customerHistory.php
+++ logic/customerHistory.php
@@ -6,19 +6,22 @@ $creditInstance = new Operations();
 $custID = $_POST['custID'];
 // $month = $_POST['month'];
 $userCredit =  $creditInstance->getCreditByCustomerId($custID);
-while($row = $userCredit->fetch_assoc()) {
-    $creditAmount = $row['credit_amount']; 
+echo $userCredit;
+// while($row = $userCredit->fetch_assoc()) {
+    // $creditAmount = $row['credit_amount']; 
     ?>
-    <div class="mt-3 row">
+    <!-- <div class="mt-3 row">
         <div class="col-md-6">
             <h6>Credit Pending: </h6>
         </div>
         <div class="col-md-6">
-            <p><?php echo $creditAmount ?? ""; ?></p>
+            <p>
+                <?php // echo $creditAmount ?? ""; ?>
+            </p>
         </div>
-    </div>    
+    </div>     -->
 <?php
-}
+// }
 ?>
 <table class="table table-striped table-bordered table-sm mt-4" cellspacing="0" width="100%">
 <thead>
