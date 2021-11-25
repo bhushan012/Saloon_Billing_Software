22430f2186b4aad8ad62fa0da4b0c1578ba66e67
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-11-25 14:11:45 +0530

    a

--- operations.php
+++ operations.php
@@ -6,13 +6,14 @@ class Operations {
         $sql = "SELECT `userId`,`username` FROM `users` WHERE `username` = '".$username."' AND `password` = '".$hashPassword."'";
         // echo $sql."<br>";
         global $conn;
-        if ($conn->query($sql) === TRUE) {
+        if ($conn->query($sql) == TRUE) {
             $result = $conn->query($sql);
             // print_r($result);
             while($row = $result->fetch_assoc()) {
                 return array("username" => $row['username'], "userId" => $row['userId'], "status" => 'true');
             }    
-        }else{
+        }
+        else{
             return array("status" => 'false');
         }
     }
