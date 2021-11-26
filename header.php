3521987dea02857a3b6a63c9de1636bdf87546ec
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-11-26 16:42:09 +0530

    echo ini get

--- header.php
+++ header.php
@@ -23,6 +23,7 @@ date_default_timezone_set('Asia/Kolkata');
 <?php 
 // if (!isset($_SESSION)) { session_start(); print_r($_SESSION);}
 session_start();
+echo ini_get('session.cookie_domain');
 // echo "USERNAME: ".$_SESSION['username'];
 // if(isset($_SESSION['test'])){
     // header('Location: '.$homeUrl.'/login.php'); 
