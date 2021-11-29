078a8407c9244c082ee30ee290bfa4bbfb28d138
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-11-29 18:45:41 +0530

    dashboard

--- forms/dashboard.php
+++ forms/dashboard.php
@@ -2,6 +2,11 @@
 include "../header.php"; 
 include "../operations.php";
 $operationInstance = new Operations();
+if($response['user_type'] == '1'){
+    header('Location: '.$formUrl.'/dashboard.php'); 
+}else{
+    header('Location: '.$formUrl.'/billing-form.php'); 
+}
 // if($_SESSION['user_type'] == '2'){
     
 // }
